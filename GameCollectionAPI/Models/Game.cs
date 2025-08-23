2a51595f9e38537e7fa33c7c824cf4ba497c3d2b
Harsche <henrique_harsche@hotmail.com>
2025-08-23 18:25:48 -0300

    Add required keyword to fields that are required

--- GameCollectionAPI/Models/Game.cs
+++ GameCollectionAPI/Models/Game.cs
@@ -5,12 +5,12 @@ namespace GameCollectionAPI.Models;
 public class Game
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Genre { get; set; }
-    public string Developer { get; set; }
-    public string Publisher { get; set; }
+    public required string Name { get; set; }
+    public required string Genre { get; set; }
+    public required string Developer { get; set; }
+    public required string Publisher { get; set; }
     public DateTime ReleaseDate { get; set; }
-    public string Platform { get; set; }
+    public required string Platform { get; set; }
 
     public void UpdateFromDto(GameCreateDto dto)
     {
