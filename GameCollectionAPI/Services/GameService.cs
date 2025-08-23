4ef974a23790ea57af0f27913dd6c7cdd66b3a68
Harsche <henrique_harsche@hotmail.com>
2025-08-23 18:14:27 -0300

    Add XML docs to GameService

--- GameCollectionAPI/Services/GameService.cs
+++ GameCollectionAPI/Services/GameService.cs
@@ -13,6 +13,11 @@ public class GameService : IGameService
         _repo = repo;
     }
 
+    /// <summary>
+    /// Creates a new game in the collection.
+    /// </summary>
+    /// <param name="createdGame">The game data to create.</param>
+    /// <returns>The created game with assigned ID.</returns>
     public async Task<GameReadDto> CreateGameAsync(GameCreateDto createdGame)
     {
         var game = Game.FromCreateDto(createdGame);
@@ -20,6 +25,11 @@ public class GameService : IGameService
         return game.ToReadDto();
     }
 
+    /// <summary>
+    /// Deletes a game from the collection by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the game to delete.</param>
+    /// <returns>True if the game was found and deleted; false if not found.</returns>
     public async Task<bool> DeleteGameAsync(int id)
     {
         var game = await _repo.GetByIdAsync(id);
@@ -33,12 +43,21 @@ public class GameService : IGameService
         return true;
     }
 
+    /// <summary>
+    /// Retrieves all games from the collection.
+    /// </summary>
+    /// <returns>A list of all games in the collection.</returns>
     public async Task<List<GameReadDto>> GetAllGamesAsync()
     {
         var games = await _repo.GetAllAsync();
         return [.. games.Select(g => g.ToReadDto())];
     }
 
+    /// <summary>
+    /// Retrieves a specific game by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the game to retrieve.</param>
+    /// <returns>The game if found; null if not found.</returns>
     public async Task<GameReadDto?> GetGameByIdAsync(int id)
     {
         var game = await _repo.GetByIdAsync(id);
@@ -51,6 +70,12 @@ public class GameService : IGameService
         return game.ToReadDto();
     }
 
+    /// <summary>
+    /// Updates an existing game with new data.
+    /// </summary>
+    /// <param name="id">The ID of the game to update.</param>
+    /// <param name="updatedGame">The updated game data.</param>
+    /// <returns>True if the game was found and updated; false if not found.</returns>
     public async Task<bool> UpdateGameAsync(int id, GameCreateDto updatedGame)
     {
         var game = await _repo.GetByIdAsync(id);
