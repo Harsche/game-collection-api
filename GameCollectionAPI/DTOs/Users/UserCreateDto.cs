3da5aeefc92dd7ad5e7544800951f022baefc823
Harsche <henrique_harsche@hotmail.com>
2026-07-01 06:16:16 -0300

    refactor(dto): update validation constraints in UserCreateDto
    - Increase minimum length of `Username` from 1 to 3
    - Add minimum length requirement of 6 characters to `Password`

--- GameCollectionAPI/DTOs/Users/UserCreateDto.cs
+++ GameCollectionAPI/DTOs/Users/UserCreateDto.cs
@@ -5,9 +5,10 @@ namespace GameCollectionAPI.DTOs.Users;
 public class UserCreateDto
 {
     [Required]
-    [StringLength(20, MinimumLength = 1)]
+    [StringLength(20, MinimumLength = 3)]
     public required string Username { get; set; }
 
     [Required]
+    [MinLength(6)]
     public required string Password { get; set; }
 }
